fc8edcdd63bac917bbe0669462ca04f0f30b1355
Moundier <garamoundier.developer@protonmail.com>
2023-11-19 19:15:24 -0300

    chore: clean code!

--- src/main/java/ufsm/csi/pilacoin/common/Constants.java
+++ src/main/java/ufsm/csi/pilacoin/common/Constants.java
@@ -2,7 +2,7 @@ package ufsm.csi.pilacoin.common;
 
 public class Constants {
 
-    public static final int PROCESSORS = getRuntime(); // 
+    public static final int PROCESSORS = getRuntime(); 
     public static final String CONST_NAME = "Casanova";
 
     public static int getRuntime() {
