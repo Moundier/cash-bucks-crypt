d525864b1cb820b2c6778bf3162cb535de3442c7
Moundier <garamoundier.developer@protonmail.com>
2023-11-24 03:02:35 -0300

    chore: commented lines!

--- src/main/java/ufsm/csi/pilacoin/config/WebSocketConfig.java
+++ src/main/java/ufsm/csi/pilacoin/config/WebSocketConfig.java
@@ -11,16 +11,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @CrossOrigin("*")
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/mining")
-            .setAllowedOrigins("http://localhost:4200")
-            .withSockJS();
-    }
+  
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/mining")
+      .setAllowedOrigins("http://localhost:4200")
+      .withSockJS();
+  }
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/ws");
-    }
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.enableSimpleBroker("/topic");
+    registry.setApplicationDestinationPrefixes("/ws");
+  }
 }
