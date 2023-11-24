5eb3d7ca3f5daedda8e438d1aa21b73fdc8c7038
Moundier <garamoundier.developer@protonmail.com>
2023-11-24 03:01:30 -0300

    chore: trim some lines!

--- src/main/java/ufsm/csi/pilacoin/model/PilaValidado.java
+++ src/main/java/ufsm/csi/pilacoin/model/PilaValidado.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ufsm.csi.pilacoin.coin.PilaCoin;
+import ufsm.csi.pilacoin.common.pilacoin.PilaCoin;
 
 @Data
 @Builder
