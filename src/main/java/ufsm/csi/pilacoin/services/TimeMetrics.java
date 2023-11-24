c4795d12ab71df38bfe5be0d328a9eb382e0cae4
Moundier <garamoundier.developer@protonmail.com>
2023-11-24 03:03:52 -0300

    fix: imports!

--- src/main/java/ufsm/csi/pilacoin/services/TimeMetrics.java
+++ src/main/java/ufsm/csi/pilacoin/services/TimeMetrics.java
@@ -1,35 +1,36 @@
 package ufsm.csi.pilacoin.services;
 
-import ufsm.csi.pilacoin.common.Colors;
+import static ufsm.csi.pilacoin.config.Config.PURPLE;
+import static ufsm.csi.pilacoin.config.Config.RESET;
+import static ufsm.csi.pilacoin.config.Config.WHITE_BOLD;
+
 import ufsm.csi.pilacoin.shared.TimeFormat;
 
 public class TimeMetrics {
 
-  private Long miningStartTime;
-  private Long miningEndTime;
-  private Long miningPeriod;
-  private Long miningPeriodHours;
-  private Long miningPeriodMinutes;
-  private Long miningPeriodSeconds;
+  private Long start;
+  private Long finish;
+  private Long totalPeriod;
+  private Long periodHours;
+  private Long periodMinutes;
+  private Long periodSeconds;
 
   public TimeMetrics(Long timerStart) {
 
-    this.miningStartTime = timerStart;
-    this.miningEndTime = System.currentTimeMillis();
-    this.miningPeriod = this.miningEndTime - this.miningStartTime;
-    this.miningPeriodSeconds = (this.miningPeriod / 1000) % 60;
-    this.miningPeriodMinutes = (this.miningPeriod / (60 * 1000)) % 60;
-    this.miningPeriodHours = this.miningPeriod / (60 * 60 * 1000);
+    this.start = timerStart;
+    this.finish = System.currentTimeMillis();
+    this.totalPeriod = (this.finish - this.start);
+    this.periodSeconds = (this.totalPeriod / 1000) % 60;
+    this.periodMinutes = (this.totalPeriod / (60 * 1000)) % 60;
+    this.periodHours = this.totalPeriod / (60 * 60 * 1000);
 
     this.getMetrics(); // Shutdown by printing out in terminal
   }
 
   private void getMetrics() {
-    System.out.println(
-        Colors.ANSI_PURPLE + "\nMining Time: " + Colors.ANSI_RESET
-            + Colors.WHITE_BOLD_BRIGHT
-            + TimeFormat.timeFormat(this.miningPeriodHours, this.miningPeriodMinutes, this.miningPeriodSeconds)
-            + Colors.ANSI_RESET);
+    System.out.print(PURPLE + "\nMining Time: " + RESET);
+    System.out.print(WHITE_BOLD);
+    System.out.print(TimeFormat.timeFormat(this.periodHours, this.periodMinutes, this.periodSeconds));
+    System.out.print(RESET);
   }
-
 }
